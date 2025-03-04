c8e75cbd4e0dd7ae21dc0684db139ad357bd05b5
Neemias Calebe <neemiascalebe2014@gmail.com>
2025-03-04 18:09:50 -0300

    Adiciona constantes de cores e tamanhos para padronização de estilos

---
+++ KalaCal/commons/index.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  mainColor: "#ff6633",
+  secondaryColor: "#ff9933",
+  defaultTextColor: "#363636",
+  defaultBackgroundColor: "#FAFAFA",
+  // TabNavigator disabled tab color
+  disabledColor: "#808080",
+};
+
+export const sizes = {
+  defaultTextSize: 18,
+};
