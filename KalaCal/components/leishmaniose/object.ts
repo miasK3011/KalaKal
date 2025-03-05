ea2be973506b744e3eb96ccf2ef57796bbbf37f8
Neemias Calebe <neemiascalebe2014@gmail.com>
2025-03-05 15:26:32 -0300

    Adiciona dados sobre Leishmaniose com informações detalhadas sobre descrição, agente etiológico, vetor, reservatório, modo de infecção, sintomatologia e aspectos epidemiológicos

---
+++ KalaCal/components/leishmaniose/object.ts
@@ -0,0 +1,45 @@
+const itens = [
+    {
+      titulo: 'Descrição',
+      descricao:
+        `\tCalazar ou a Leishmaniose Visceral (LV) é uma doença zoonótica que causa um conjunto de síndromes clínicas em humanos que podem comprometer pele, mucosa e vísceras (OPAS,2019).`,
+    },
+    {
+      titulo: 'Agente Etiológico',
+      descricao: `\tAs leishmanioses são causadas por protozoários da ordem Kinetoplastida, família Trypanosomatidae, gênero Leishmania (L.).\n\n\tNo Brasil, as espécies causadoras da leishmaniose visceral fazem parte do complexo L. donovani que são classificadas em três espécies: L. chagasi, L. infantum, L. donovani, diferenciando-se por seus vetores, hospedeiros reservatórios e patologia. A Leishmania (L.) infantum está, comumente responsável como a espécie no Brasil.\n\n(MAURÍCIO; STOHARD; MILES, 2000; LUKES et al., 2007; OLIVEIRA et al., 2009; RAYMOND et al., 2012).`
+    },
+    {
+      titulo: 'Vetor',
+      descricao: `\tO principal vetor da América Central e do Sul da leishmaniose visceral (LV) causada por Leishmania infantum é a fêmea de insetos de flebotomíneos da espécie Lutzomyia longipalpis.\n\n\tEsses vetores tem metamorfose completa com estágios de: ovo, larva, pupa e adultos.\n\n As fêmeas adultas de L. longipalpis têm atividade crepuscular e noturna, embora também possam estar ativos durante o dia. E as estratégias de controle de vigilância do vetor são direcionadas aos estágios adultos. (OPAS,2019; SOUSA-PAULA et al.,2020).\n\n\tSaiba mais sobre vetor ( link para abrir: https://www.who.int/leishmaniasis/disease/vector/en/)
+      `
+    },
+    {
+      titulo: 'Reservatório',
+      descricao: `São aqueles hospedeiros vertebrados que mantêm o parasita na natureza e, portanto, certos vetores são infectados por eles e o ciclo de transmissão continua a existir. Os reservatórios são domésticos e selvagens. Entre os selvagens estão: marsupiais, urso-preguiça, tamanduá, menor, raposa e roedores. O doméstico mais importante para L. (L) infantum está o cão (OPAS,2019).\n\n\tQuer conhecer mais sobre os tipos de reservatórios (link para abrir: https://iris.paho.org/bitstream/handle/10665.2/50524/9789275320631_spa.pdf?sequence=1&isAllowed=y)`
+    },
+    {
+      titulo: 'Modo de infecção',
+      descricao: `A infecção ocorre quando formas promastigotas infectantes são inoculadas no tecido do hospedeiro durante o repasto sanguíneo do inseto vetor. Após escaparem da lise mediada pelo sistema complemento, as promastigotas de Leishmania invadem rapidamente a célula alvo para iniciar a fase intracelular da infecção, já que uma vez internalizadas aos fagócitos, esses parasitos modulam o microambiente estabelecendo a infecção (DESJEUX, 2004).\n\n\tNo macrófago, os parasitos perdem o flagelo, tornando-se arredondados ou ovoides e são denominadas de amastigotas, formas que residem no interior dos fagolisossomos, onde sobrevivem e se multiplicam. Macrófagos infectados se rompem e liberam amastigotas com potencial infectante, contaminando macrófagos vizinhos. Em um novo repasto sanguíneo, o inseto vetor ingere amastigotas infectados. Essas amastigotas, ao chegarem ao intestino do inseto, transformam-se em promastigotas e rapidamente se multiplicam. O inseto, ao exercer um novo hematofagismo, inocula formas infectantes e o ciclo vertebrado se completa.\n\n(SACKS; KAMHAWI, 2001; OLIVEIRA et al., 2009)`
+    },
+    {
+      titulo: 'Sintomatologia',
+      descricao: `
+      O calazar pode se manifestar com um quadro clínico leve, moderado ou grave, no entanto estudos relatam que a maioria dos indivíduos infectados é assintomática e, podem não serem acompanhando pelo sistema de vigilância ou seguirem um tratamento. No estágio inicial a doença pode ser facilmente confundida com diferentes processos infecciosos. Uma proporção de indivíduos infectados apresentará sintomas que evoluem insidiosamente, com esplenomegalia, febre irregular, anemia ou pancitopenia, perda de peso e fraqueza ocorrendo progressivamente ao longo de um período de semanas ou mesmo meses. (OPAS, 2019; FABIANA ALVES et. al, 2018).
+      `
+    },
+    {
+      titulo: 'Aspectos Epidemiológicos',
+      descricao: `
+      As leishmanioses são consideradas doenças endêmicas em 102 países no mundo e segundo a Organização Mundial de Saúde (OMS) é a sexta endemia mais importante, e a segunda causa de morte entre as infecções parasitárias (OMS, 2013; NOGUEIRA; MONTEIRO; ESCOBAR, 2013; WHO, 2015; OPAS, 2019). Cerca de 12 milhões de pessoas são clinicamente afetadas e 1,5 a 2,0 milhões de novos casos ocorrem a cada ano, sendo em média 1,5 milhões de leishmaniose tegumentar (LT) e 200.000 – 400.000, leishmaniose visceral (LV).
+  
+      Nas Américas, 96% dos registros de LV, referente ao período de 2018, foram notificados no Brasil, com uma incidência 5.05 casos para 100.000 habitantes (OPAS,2019b). Na década de 1990, aproximadamente 90% dos casos notificados de LV no Brasil ocorreram na região Nordeste. Em 2012, decorrente do processo de expansão da doença para outras regiões do Brasil, modificou a situação epidemiológica da LV no país, sendo a região Nordeste responsável por 43,1% dos casos (BRASIL, 2019).
+      
+      No estado do Piauí, a primeira grande epidemia urbana de LV ocorreu entre os anos 1980 a 1986, com um maior de casos em 1983 e 1984 (Costa, 1990). Segundo Werneck (2008), ao avaliar a efetividade das estratégias de controle de LV na cidade de Teresina-PI, constatou a segunda grande epidemia no estado no período de 1992 a 1995, com pico da taxa de incidência nos anos de 1993 e 1994. Drumond (2011), ao analisar quarenta anos de LV no Piauí, enfatizou o aumento da incidência entre 2003 e 2004, destacando um movimento cíclico da doença com pico a cada 10 anos.
+      
+      Em uma recente verificação no banco de dados, no período de 2008 a 2018 no Piauí observou nos anos de 2014 e 2008 foram constatadas as maiores taxas de incidência da doença (8,9/100.000 hab. e 8,9/100.000 hab., respectivamente). Além disso, ao analisar o ranking da carga das doenças tropicais negligenciadas no Piauí, comparando os anos de 2008 e 2017, constatamos que as leishmanioses ocupam a maior carga dentro do estado, mantendo-se na 1º posição em todo o período estudado.
+      
+      `
+    }
+  ];
+
+export default itens
